2f8c50b713b12d2475afaa3aa85faea5476faaee
Tomas <tomas.salgado@ucr.ac.cr>
2026-04-30 03:04:45 +0000

    Modificar README y Integral

--- Integral.py
+++ Integral.py
@@ -1,1 +1,27 @@
-Python code
+import numpy as np
+import matplotlib.pyplot as plt
+
+def func(xVal):
+    return np.sin(xVal*xVal)
+
+def gaussOriginal(N):
+    xVal, weight=np.polynomial.legendre.leggauss(N)
+    return xVal, weight
+
+def gaussInLimit(xInit,xFinal,xVal,weight):
+    return 0.5*(xFinal-xInit)*xVal+0.5*(xFinal+xInit), 0.5*(xFinal-xInit)*weight
+
+nVals=np.array([1,2,3,4,5,6,7,8,9,10])
+integVals=np.zeros(nVals.size)
+
+for i in range(nVals.size):
+    gaussN=gaussOriginal(nVals[i])
+    gaussNadj=gaussInLimit(0.0,np.pi,gaussN[0],gaussN[1])
+    integVals[i]+=np.sum(func(gaussNadj[0])*gaussNadj[1])
+
+plt.plot(nVals,integVals)
+plt.grid()
+plt.xlabel("Valor de N")
+plt.ylabel("Aproximación de la integral")
+
+plt.show
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Problema-de-Integral
-Este repositorio registra la solución de una integral usando el método de cuadratura gaussiana.
+# Tarea 2: Aproximación integral.
+
+Con este trabajo se quiere aproximar el valor de la siguiente integral por medio de la técnica de cuadratura gaussiana:
+
+$I=\int_{0}^{\pi}sin(x^2)\ dx$
+
+
+
