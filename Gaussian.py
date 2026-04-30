b63349cb219e34b999a4a14dbc8ec767ac67be84
Tomas <tomas.salgado@ucr.ac.cr>
2026-04-30 23:07:43 +0000

    modifico gaussian

--- Gaussian.py
+++ Gaussian.py
@@ -84,18 +84,23 @@ def gaussInLimit(xInit, xFinal, xVal, weight):
         0.5 * (xFinal - xInit) * weight
     )
 
+def main()
+    """
+    Realiza una gráfica de las aproximaciones de la integral respecto a los valoenteros entre 1 y 10.  
+    
+    """    
+    nVals = np.array([1,2,3,4,5,6,7,8,9,10])
+    integVals = np.zeros(nVals.size)
 
-nVals = np.array([1,2,3,4,5,6,7,8,9,10])
-integVals = np.zeros(nVals.size)
+    for i in range(nVals.size):
+        gaussN = gaussOriginal(nVals[i])
+        gaussNadj = gaussInLimit(0.0, np.pi, gaussN[0], gaussN[1])
+        integVals[i] += np.sum(func(gaussNadj[0]) * gaussNadj[1])
 
-for i in range(nVals.size):
-    gaussN = gaussOriginal(nVals[i])
-    gaussNadj = gaussInLimit(0.0, np.pi, gaussN[0], gaussN[1])
-    integVals[i] += np.sum(func(gaussNadj[0]) * gaussNadj[1])
+    plt.plot(nVals, integVals)
+    plt.grid()
+    plt.xlabel("Valor de N")
+    plt.ylabel("Aproximación de la integral")
+    plt.show()
 
-plt.plot(nVals, integVals)
-plt.grid()
-plt.xlabel("Valor de N")
-plt.ylabel("Aproximación de la integral")
-
-plt.show()
+main()
